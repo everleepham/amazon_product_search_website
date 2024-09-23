da5d1de08759f09d5731032a295e211bd81c3cba
Phuong Khanh Pham <phuong-khanh.pham@epita.fr>
2024-09-23 15:49:44 +0200

    first commit

--- main.js
+++ main.js
@@ -1,2 +1,30 @@
 import "./css/bootstrap.min.css"
 import "./js/bootstrap.bundle.min"
+
+const URL = "https://jsonplaceholder.typicode.com/users"
+fetch(URL)
+	.then((res) => {
+		if (!res.ok) {
+			return (container.textContent = "no user found with this data")
+        }
+        res.json().then((json) => {
+            console.log(json)
+            handleData(json) 
+        })
+    })
+    
+
+function handleData(json) {
+    json.forEach(user => {
+        document.querySelector(".dynamic_data").innerHTML += `<div class="col">
+        <artricle class="card">
+            <div class="card-body">
+                <p>${user.name}</p>
+                <p>${user.email}</p>
+            </div>
+        </artricle>
+    </div>`
+    });
+    console.log(typeof json)
+}
+
